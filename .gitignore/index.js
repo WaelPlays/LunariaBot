d24cd24d75ea46219c2fb4b9314e9100de00c5d2
kriseabl <34816229+kriseabl@users.noreply.github.com>
2017-12-24 17:20:24 +0000

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js);
+
+var bot = new Discord.Client();
+
+bot.on("ready", function() {
+    bot.user.setGame("LunariaBot, !help");
+    console.log("Le bot a été coder par krise");
+    });
+
+    bot.login("Mzk0NTA2ODI2Nzg1NDg4OTA2.DSFj7g.K1AtYXh6beb49pm-1jZLlHVLHnM");
+        
